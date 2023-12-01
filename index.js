f26879d4ef4cfde5e590ffd8e0f8c017d14eb970
Alexander Station <widmark.alexander@gmail.com>
2023-12-01 17:19:46 +0100

    fixed gym pics and added section

--- index.js
+++ index.js
@@ -13,34 +13,3 @@ function showSlides() {
     setTimeout(showSlides, 6000);
 }
 
-
-
-
-
-
-
-
-
-
-
-/*
-
-   window.onload = function (){
-let image = document.getElementById('t-arena')
-let images = ['img/football-training.jpg', 'img/football-arena.jpg']
-let index = 0;
-const interval = 8000;
-function slide() {
-    image.src = images[index++%images.length]
-}
-setInterval(slide, interval);
-}
-*/
-
-
-/*
-setInterval(function (){
-    let random = Math.floor(Math.random() * 2)
-    image.src = images[random];
-}, 3000);
-*/
