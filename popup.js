9d12d22fc0e98cbdb20683722235409760ab05ba
Alexander Station <widmark.alexander@gmail.com>
2023-12-11 17:02:11 +0100

    added popup and other minor fixes

---
+++ popup.js
@@ -0,0 +1,38 @@
+// Wrap your code in a DOMContentLoaded event listener to ensure the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    // Check if the element with ID 'closepop' exists
+    const closePopButton = document.getElementById("closepop");
+
+    // If the element exists, add the event listener
+    if (closePopButton) {
+        closePopButton.addEventListener("click", function () {
+            document.querySelector(".popup").style.display = "none";
+        });
+    } else {
+        console.warn("Element with ID 'closepop' not found.");
+    }
+
+    // Check if the element with ID 'cta' exists
+    const ctaButton = document.getElementById("cta");
+
+    // If the element exists, add the event listener
+    if (ctaButton) {
+        ctaButton.addEventListener("click", function () {
+            document.querySelector(".popup").style.display = "flex";
+        });
+    } else {
+        console.warn("Element with ID 'cta' not found.");
+    }
+
+    // Check if the element with ID 'ctajoin' exists
+    const ctaJoinButton = document.getElementById("ctajoin");
+
+    // If the element exists, add the event listener
+    if (ctaJoinButton) {
+        ctaJoinButton.addEventListener("click", function () {
+            document.querySelector(".popup").style.display = "flex";
+        });
+    } else {
+        console.warn("Element with ID 'ctajoin' not found.");
+    }
+});
